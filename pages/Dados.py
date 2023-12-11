f4b3716dc15b46b9407d06fe55f4771599a51966
YOSHIAKI SAKAGAMI <54770948+sakagamiyoshiaki@users.noreply.github.com>
2023-12-11 15:03:48 -0300

    Update Dados.py

--- pages/Dados.py
+++ pages/Dados.py
@@ -7,3 +7,21 @@ doc_ref = db.collection("stations").document("maciambu")
 
 # Then get the data at that reference.
 doc = doc_ref.get()
+
+
+LOGGER = get_logger(__name__)
+
+def run():
+
+
+    st.write("#Bem Vindo ")
+
+    st.sidebar.success("Selecione a Análise.")
+
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
+
+
+
+if __name__ == "__main__":
+    run()
