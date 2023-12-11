53750275741ce38ed8b02a69d730d32de08fd93b
YOSHIAKI SAKAGAMI <54770948+sakagamiyoshiaki@users.noreply.github.com>
2023-12-11 15:10:06 -0300

    Update 4_Dados.py

--- pages/4_Dados.py
+++ pages/4_Dados.py
@@ -1,10 +1,8 @@
-
 import streamlit as st
 from google.cloud import firestore
 
-
 # Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("../pages/firestore-key.json")
+db = firestore.Client.from_service_account_json("firestore-key.json")
 
 # Create a reference to the Google post.
 doc_ref = db.collection("stations").document("maciambu")
@@ -12,20 +10,7 @@ doc_ref = db.collection("stations").document("maciambu")
 # Then get the data at that reference.
 doc = doc_ref.get()
 
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
 
-LOGGER = get_logger(__name__)
-
-def run():
-
-
-    st.write("#Bem Vindo ")
-
-    st.sidebar.success("Selecione a Análise.")
-
-    st.write("The id is: ", doc.id)
-    st.write("The contents are: ", doc.to_dict())
-
-
-
-if __name__ == "__main__":
-    run()
