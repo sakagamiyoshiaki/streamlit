baf1b25c0493313136eba399f979796ec6d7a932
YOSHIAKI SAKAGAMI <54770948+sakagamiyoshiaki@users.noreply.github.com>
2023-12-11 15:03:15 -0300

    Create Dados.py

---
+++ pages/Dados.py
@@ -0,0 +1,9 @@
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("stations").document("maciambu")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
