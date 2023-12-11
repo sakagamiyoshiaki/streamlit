9fb74190995a417a87fc1503d3834c55eb8fb058
YOSHIAKI SAKAGAMI <54770948+sakagamiyoshiaki@users.noreply.github.com>
2023-12-11 09:46:20 -0300

    Update 2_Maciambu.py

--- pages/2_Maciambu.py
+++ pages/2_Maciambu.py
@@ -26,13 +26,11 @@ def plotting_demo():
     st.button("Re-run")
 
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="Kitezone - Maciambu - IPALHO4", page_icon="📈")
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
 st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+    """Monitoramento das condições meteorológicas em Maciambú!"""
 )
 
 plotting_demo()
