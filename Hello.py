5086dbc345975c45c0970b097f80cb80740a5af8
YOSHIAKI SAKAGAMI <54770948+sakagamiyoshiaki@users.noreply.github.com>
2023-12-11 15:11:44 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
+from google.cloud import firestore
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("stations").document("maciambu")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
 
 LOGGER = get_logger(__name__)
 
@@ -13,6 +24,10 @@ def run():
     st.write("#Bem Vindo ")
 
     st.sidebar.success("Selecione a Análise.")
+    # Let's see what we got!
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
+
 
     st.markdown(
         """
